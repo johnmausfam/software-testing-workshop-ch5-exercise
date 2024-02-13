91bd70e8c5c492c1704cf919d32aff2fcaf3cbf4
fam <fam1923@gmail.com>
2024-02-13 14:43:59 +0800

    try to kill mutations

--- __tests__/ch5.test.js
+++ __tests__/ch5.test.js
@@ -1,7 +1,11 @@
 const { union_set } = require('../src/ch5_exercise');
 
+
 describe('test for union_set', () => {
-    const testData = [{ a: new Set([1, 2, 3]), b: new Set([1, 2, 3, 4, 5]), r: [1, 2, 3, 4, 5] }];
+    const testData = [
+        { a: new Set([1, 2, 3]), b: new Set([1, 2, 3, 4, 5]), r: [1, 2, 3, 4, 5] },
+        { a: new Set([1, 2, 3]), b: new Set([4, 5]), r: [1, 2, 3, 4, 5] }
+    ];
 
     it.each(testData)('測試結果為聯集Set', ({ a, b, r }) => {
         const test_a = a,
@@ -30,5 +34,12 @@ describe('test for union_set', () => {
         expectedResults.forEach(function (expectedResult) {
             expect(result.has(expectedResult)).toBe(true);
         });
+    });
+    it('當b傳入null時，不可以產生exception', () => {
+        const test_a = new Set([1, 2, 3]),
+            test_b = null;
+
+        const tester = () => union_set(test_a, test_b);
+        expect(tester).not.toThrow();
     });
 });
